f96d8ed6e1495878e907ec7c4f63b4fe0fbbf2d7
Bruno Haetinger <brunohaetinger@gmail.com>
2020-01-07 00:03:13 -0300

    Move components into a folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app-component/app.component';
+import { AppComponent } from './components/app-component/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { HomePageComponent } from './home-page/home-page.component';
-import { MainSelectionComponent } from './main-selection/main-selection.component';
-import { MoviesHorizontalListComponent } from './movies-horizontal-list/movies-horizontal-list.component';
-import { MovieCardListComponent } from './movie-card-list/movie-card-list.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { PlayMovieComponent } from './play-movie/play-movie.component';
-import { ProfileComponent } from './profile/profile.component';
-import { UserAvatarComponent } from './user-avatar/user-avatar.component';
-import { MetricsComponent } from './metrics/metrics.component';
-import { TopUserListComponent } from './top-user-list/top-user-list.component';
-import { MostWatchedGenresComponent } from './most-watched-genres/most-watched-genres.component';
-import { UserCardComponent } from './user-card/user-card.component';
-import { GenreCardListComponent } from './genre-card-list/genre-card-list.component';
-import { GenreCardComponent } from './genre-card/genre-card.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MainSelectionComponent } from './components/main-selection/main-selection.component';
+import { MoviesHorizontalListComponent } from './components/movies-horizontal-list/movies-horizontal-list.component';
+import { MovieCardListComponent } from './components/movie-card-list/movie-card-list.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { PlayMovieComponent } from './components/play-movie/play-movie.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UserAvatarComponent } from './components/user-avatar/user-avatar.component';
+import { MetricsComponent } from './components/metrics/metrics.component';
+import { TopUserListComponent } from './components/top-user-list/top-user-list.component';
+import { MostWatchedGenresComponent } from './components/most-watched-genres/most-watched-genres.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { GenreCardListComponent } from './components/genre-card-list/genre-card-list.component';
+import { GenreCardComponent } from './components/genre-card/genre-card.component';
 
 @NgModule({
   declarations: [
