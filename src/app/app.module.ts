8a5bb2b583a3139ddc4e3903892a2b508b708b48
Bruno Haetinger <brunohaetinger@gmail.com>
2020-01-06 23:31:05 -0300

    Move toolbar to home page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-component/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
-// import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppToolbarComponent
+    AppToolbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
