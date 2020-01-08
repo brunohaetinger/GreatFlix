311e2edf82e59452ebfdaf0ae2a406985c156979
Bruno Haetinger <brunohaetinger@gmail.com>
2020-01-08 03:44:06 -0300

    Add Metrics component code

--- src/app/components/metrics/metrics.component.ts
+++ src/app/components/metrics/metrics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Movie from 'src/app/types/Movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'metrics',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./metrics.component.sass']
 })
 export class MetricsComponent implements OnInit {
-
-  constructor() { }
+  mostWatched: Movie[]
+  countries: string[]
+  constructor(moviesService: MoviesService) { 
+    this.mostWatched = moviesService.getMostWatchedMovies();
+    this.countries = moviesService.getCountries();
+  }
 
   ngOnInit() {
   }
