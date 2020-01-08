b92cb8cb3c3e251ea47bf9a6f31f52044ef69c1b
Bruno Haetinger <brunohaetinger@gmail.com>
2020-01-08 03:44:53 -0300

    Set play movie component

--- src/app/components/play-movie/play-movie.component.ts
+++ src/app/components/play-movie/play-movie.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { MoviesService } from 'src/app/services/movies.service';
+import Movie from 'src/app/types/Movie';
 
 @Component({
-  selector: 'play-movie',
-  templateUrl: './play-movie.component.html',
-  styleUrls: ['./play-movie.component.sass']
+  selector: "play-movie",
+  templateUrl: "./play-movie.component.html",
+  styleUrls: ["./play-movie.component.sass"]
 })
 export class PlayMovieComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  movie: Movie;
+  constructor(route: ActivatedRoute, moviesService: MoviesService) {
+    const id: string = route.snapshot.params["id"];
+    this.movie = moviesService.getMovieByID(id);
   }
 
+  ngOnInit() {}
 }
