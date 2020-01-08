7afd09f29ab5bfd5c9497fea40b67b215d79c500
Haetinger <bruno.haetinger@sap.com>
2020-01-08 18:41:03 -0300

    Set play movie metrics

--- src/app/components/play-movie/play-movie.component.ts
+++ src/app/components/play-movie/play-movie.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { MoviesService } from 'src/app/services/movies.service';
 import Movie from 'src/app/types/Movie';
+import { DaoService } from 'src/app/services/dao.service';
+import { UserService } from 'src/app/services/user.service';
+import User from 'src/app/types/User';
 
 @Component({
   selector: "play-movie",
@@ -10,10 +13,20 @@ import Movie from 'src/app/types/Movie';
 })
 export class PlayMovieComponent implements OnInit {
   movie: Movie;
-  constructor(route: ActivatedRoute, moviesService: MoviesService) {
+  constructor(route: ActivatedRoute, moviesService: MoviesService, private dao: DaoService, private userService: UserService) {
     const id: string = route.snapshot.params["id"];
     this.movie = moviesService.getMovieByID(id);
   }
 
   ngOnInit() {}
+
+  playMovieClick(){
+    const user: User = this.userService.getCurrentUser();
+    this.dao.insertUserView(user.username, this.movie.imdbID);
+    
+    //set auxiliary Metrics
+    this.dao.incrementGenreWatchedCounter(this.movie.genres);
+    this.dao.incrementViewByCountryCounter(this.movie.imdbID, user.country);
+    this.dao.incrementTopUserCounter(user.username);
+  }
 }
