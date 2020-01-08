fb3a9e84089aa63ab1388e70b2171969bfa94771
Bruno Haetinger <brunohaetinger@gmail.com>
2020-01-08 03:45:03 -0300

    Define movie card comp

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit, Input} from '@angular/core';
-import { MoviesService } from 'src/app/services/movies.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { MoviesService } from "src/app/services/movies.service";
 
 @Component({
-  selector: 'movie-card',
-  templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.sass']
+  selector: "movie-card",
+  templateUrl: "./movie-card.component.html",
+  styleUrls: ["./movie-card.component.sass"]
 })
 export class MovieCardComponent implements OnInit {
-  @Input() movie
-  pic
+  @Input() movie;
+  pic;
   constructor(public moviesService: MoviesService) {
-    console.log("ENtrei!!")
+    
   }
-  
+
   ngOnInit() {
-    if(!this.pic && this.movie){
+    if (!this.pic && this.movie) {
       this.moviesService.getPoster(this.movie.imdbID).then(poster => {
         this.pic = poster;
-      })
+      });
     }
   }
-
-  
-
 }
